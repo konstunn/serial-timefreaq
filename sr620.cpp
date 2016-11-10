0141bfd85402071ad703bb4cc73c0c0649a2ea5b
Artem Karaush <karaush.a@gmail.com>
2016-11-10 13:34:55 +0700

    Microsoft does not like standard snprintf function name

--- sr620.cpp
+++ sr620.cpp
@@ -84,7 +84,7 @@ HANDLE sr620_open_config_port_by_name(
 		//STF_RETURN_ERROR(hport);
 
 	char sr_mode_str[255];
-	snprintf((char*) sr_mode_str, 255,
+	_snprintf((char*) sr_mode_str, 255,
 		"MODE0;CLCK1;CLKF%1d;LOCL1;TCPL0;SRCE0;AUTM0;ARMM1;SIZE1"
 		"LEVL2,1;LEVL3,1;TSLP2,0;TSLP3,0\n",
 		sr_ext_clk_freq);
